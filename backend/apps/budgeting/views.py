a240ade023f5ca4374c090a1ac5f99c8ab82debe
gleanse <auldeyjerusalem2@gmail.com>
2025-03-25 00:55:03 +0800

    set up django rest frameworks
    i installed djangorestframeworks and test an api i also create a view and serialization for testng purposes only i also use an api to test which is the home view

--- backend/apps/budgeting/views.py
+++ backend/apps/budgeting/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
-# Create your views here.
+# NOTE: api testing only subject to delete 
+
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from .models import Budget
+from .serializers import BudgetSerializer
+
+class HomeView(APIView):
+    def get(self, request):
+        return Response({"message": "Welcome!! This is temporary hompage of the app"})
