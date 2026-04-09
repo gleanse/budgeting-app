756c41b45933b8d85f1a38c4150be21df0572561
Auldey Glen <auldeyjerusalem2@gmail.com>
2026-04-09 17:39:37 +0800

    schema: prepare accounts for tracking of transactions

--- fastapi-version/app/models.py
+++ fastapi-version/app/models.py
@@ -1,18 +1,19 @@
-from sqlmodel import Field, SQLModel, Relationship
+from sqlmodel import Field, SQLModel, Relationship, UniqueConstraint
 from datetime import datetime, timezone
 import bcrypt
 
 
 class User(SQLModel, table=True):
+    __tablename__ = "users"
+
     id: int | None = Field(default=None, primary_key=True)
     username: str = Field(index=True)
     password_hash: str
 
-    # RELATIONSHIP
-    # one to many relationship since one user can have so many incomes thats why its foreignkey to Income table
     incomes: list["Income"] = Relationship(back_populates="user")
     expenses: list["Expense"] = Relationship(back_populates="user")
     categories: list["Category"] = Relationship(back_populates="user")
+    accounts: list["Account"] = Relationship(back_populates="user")
 
     # hash password and store it
     def set_password(self, password: str):
@@ -28,41 +29,61 @@ class User(SQLModel, table=True):
         )
 
 
-class Income(SQLModel, table=True):
-    id: int | None = Field(default=None, primary_key=True)
-    # gt means greater than to make sure it wont stored negatives invalid numbers
-    amount: float = Field(gt=0)
-    category_id: int | None = Field(default=None, foreign_key="category.id")
-    description: str
-    # default_factory for dynamic values, used lambda to encapsulate the datetime stamp function that only run everytime its recorded or data is being inserted
-    date_time: datetime = Field(default_factory=lambda: datetime.now(timezone.utc))
-    user_id: int = Field(foreign_key="user.id")
-
-    # RELATIONSHIP
-    user: User | None = Relationship(back_populates="incomes")
-    category: "Category" = Relationship(back_populates="incomes")
-
-
-class Expense(SQLModel, table=True):
-    id: int | None = Field(default=None, primary_key=True)
-    amount: float = Field(gt=0)
-    category_id: int | None = Field(default=None, foreign_key="category.id")
-    description: str
-    date_time: datetime = Field(default_factory=lambda: datetime.now(timezone.utc))
-    user_id: int = Field(foreign_key="user.id")
-
-    # RELATIONSHIP
-    user: User | None = Relationship(back_populates="expenses")
-    category: "Category" = Relationship(back_populates="expenses")
-
-
 class Category(SQLModel, table=True):
+    __tablename__ = "categories"
+    __table_args__ = (UniqueConstraint("user_id", "name"),)
+
     id: int | None = Field(default=None, primary_key=True)
     name: str = Field(index=True)
     type: str = Field()
-    user_id: int = Field(foreign_key="user.id")
+    user_id: int = Field(foreign_key="users.id")
 
-    # RELATIONSHIP
-    user: User | None = Relationship(back_populates="categories")
+    user: User = Relationship(back_populates="categories")
     incomes: list["Income"] = Relationship(back_populates="category")
     expenses: list["Expense"] = Relationship(back_populates="category")
+
+
+class Account(SQLModel, table=True):
+    __tablename__ = "accounts"
+    __table_args__ = (UniqueConstraint("user_id", "name"),)
+
+    id: int | None = Field(default=None, primary_key=True)
+    name: str = Field(index=True)
+    user_id: int = Field(foreign_key="users.id")
+
+    user: User = Relationship(back_populates="accounts")
+    incomes: list["Income"] = Relationship(back_populates="account")
+    expenses: list["Expense"] = Relationship(back_populates="account")
+
+
+class Income(SQLModel, table=True):
+    __tablename__ = "incomes"
+
+    id: int | None = Field(default=None, primary_key=True)
+    # gt(greater than) to avoid negative value
+    amount: float = Field(gt=0)
+    category_id: int | None = Field(default=None, foreign_key="categories.id")
+    description: str
+    date_time: datetime = Field(default_factory=lambda: datetime.now(timezone.utc))
+    user_id: int = Field(foreign_key="users.id")
+    account_id: int = Field(foreign_key="accounts.id")
+
+    user: User = Relationship(back_populates="incomes")
+    category: Category | None = Relationship(back_populates="incomes")
+    account: Account = Relationship(back_populates="incomes")
+
+
+class Expense(SQLModel, table=True):
+    __tablename__ = "expenses"
+
+    id: int | None = Field(default=None, primary_key=True)
+    amount: float = Field(gt=0)
+    category_id: int | None = Field(default=None, foreign_key="categories.id")
+    description: str
+    date_time: datetime = Field(default_factory=lambda: datetime.now(timezone.utc))
+    user_id: int = Field(foreign_key="users.id")
+    account_id: int = Field(foreign_key="accounts.id")
+
+    user: User = Relationship(back_populates="expenses")
+    category: Category | None = Relationship(back_populates="expenses")
+    account: Account = Relationship(back_populates="expenses")
