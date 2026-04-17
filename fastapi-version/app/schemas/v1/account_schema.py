b9d9c0e2c5d12287c1d1ee1656deab0093cacc78
Auldey Glen <auldeyjerusalem2@gmail.com>
2026-04-17 14:37:27 +0800

    extend schema for account

--- fastapi-version/app/schemas/v1/account_schema.py
+++ fastapi-version/app/schemas/v1/account_schema.py
@@ -9,6 +9,19 @@ class AccountResponse(BaseModel):
     id: int
     name: str
 
-
 class AccountBalanceResponse(BaseModel):
     total_balance: float
+
+
+class AccountCreateResponse(BaseModel):
+    message: str = "Account created successfully"
+    account: AccountResponse
+
+class AccountDelete(BaseModel):
+    id: int
+    name: str
+
+
+class AccountDeleteResponse(BaseModel):
+    message: str = "Account deleted successfully"
+    deleted_item: AccountDelete
